0d0cb916b6a3daa63f07255d3c519ba97f913817
mynameisrim <rim.boukari@esprit.tn>
2022-02-20 22:43:02 +0100

    update frontend

--- src/Controller/ProgController.php
+++ src/Controller/ProgController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\LogementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,11 @@ class ProgController extends AbstractController
     /**
      * @Route("/prog", name="prog")
      */
-    public function index(): Response
+    public function index(LogementRepository $logementRepository): Response
     {
         return $this->render('prog/index.html.twig', [
             'controller_name' => 'ProgController',
+            'logements' => $logementRepository->findAll()
         ]);
     }
 }
